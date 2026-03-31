d210bb6042889cf28122b45c896d82ccc5856a5d
torika <imzenya5266@gmail.com>
2026-03-31 22:54:19 +0700

    Done

--- bin/midTerm.dart
+++ bin/midTerm.dart
@@ -9,6 +9,7 @@ void main(){
   double principle = loanAmount/loanTerm;
   double interest = (loanAmount*rate)/100;
   double balance = loanAmount-principle;
+  print(interest);
   DateTime dateOfPayment; 
   double payment = principle+interest;
   String fLaon = loanAmount.toStringAsFixed(2);
@@ -20,29 +21,52 @@ void main(){
   stdout.writeln('N'+'Date Of Payment'.padLeft(30)+'Payment'.padLeft(30)+'Interest'.padLeft(30)+'Principle'.padLeft(30)+'Balance'.padLeft(30));
   stdout.writeln('-'*150);
 
-  int addDay=0;
-  for(int i =1; i<=loanTerm;i++){
-    String fInterest = interest.toStringAsFixed(2);
-    String fPrinciple = principle.toStringAsFixed(2);
-    String fBalance = balance.toStringAsFixed(2);
-    String fPayment = payment.toStringAsFixed(2);
+  for(int i = 1; i <= loanTerm; i++) {
 
-    
-    dateOfPayment = DateTime(borrowDate.year, borrowDate.month+i , borrowDate.day,);
-    // stdout.write(dateOfPayment.weekday);
-    if(dateOfPayment.weekday==6){
-      dateOfPayment = DateTime(borrowDate.year, borrowDate.month+i , borrowDate.day+2,);
-      borrowDate= borrowDate.add(Duration(days: 2));
-    }else if(dateOfPayment.weekday==7){
-      dateOfPayment = DateTime(borrowDate.year, borrowDate.month+i , borrowDate.day+1,);
-      borrowDate = borrowDate.add(Duration(days: 1));
-    }
-    balance-=principle;
-    loanAmount =balance;
-    interest =(loanAmount *rate)/100;
-    payment = interest+principle;
-    stdout.writeln('$i'+'${dateFormat.format(dateOfPayment)}'.padLeft(30)+'$fPayment'.padLeft(30)+'$fInterest'.padLeft(30)+'$fPrinciple'.padLeft(30)+'$fBalance'.padLeft(30));
-    stdout.writeln('-'*150);
+  int dayAdd = 0;
+
+  dateOfPayment = DateTime(
+    borrowDate.year,
+    borrowDate.month + i,
+    borrowDate.day,
+  );
+
+  if (dateOfPayment.weekday == 6) {
+    dateOfPayment = DateTime(
+      borrowDate.year,
+      borrowDate.month + i,
+      borrowDate.day + 2,
+    );
+    borrowDate = borrowDate.add(Duration(days: 2));
+    dayAdd = 2;
+  } else if (dateOfPayment.weekday == 7) {
+    dateOfPayment = DateTime(
+      borrowDate.year,
+      borrowDate.month + i,
+      borrowDate.day + 1,
+    );
+    borrowDate = borrowDate.add(Duration(days: 1));
+    dayAdd = 1;
   }
+
+  
+  double newInterest = ((loanAmount * rate) / (100 * 30)) * dayAdd;
+
+  
+  interest = (loanAmount * rate) / 100 + newInterest;
+  payment = interest + principle;
+  
+
+  String fInterest = interest.toStringAsFixed(2);
+  String fPrinciple = principle.toStringAsFixed(2);
+  String fBalance = balance < 0 ? "0.00" : balance.toStringAsFixed(2);
+  String fPayment = payment.toStringAsFixed(2);
+
+  stdout.writeln(
+    '$i''${dateFormat.format(dateOfPayment)}'.padLeft(30)+'$fPayment'.padLeft(30)+'$fInterest'.padLeft(30) +'$fPrinciple'.padLeft(30) +'$fBalance'.padLeft(30));
+  stdout.writeln('-' * 150);
+  loanAmount = balance;
+  balance = loanAmount - principle;
+}
 
 }
